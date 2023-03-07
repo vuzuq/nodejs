a19b92df563f333c450178343fb6c8b83525ad83
vuddung <vudakzuq@gmail.com>
2023-03-07 14:38:38 +0700

    node

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -1,6 +1,7 @@
-import { Express } from "express-serve-static-core";
+import express from 'express'
 
 const configViewEngine = (app) => {
+    app.use(express.static('./src/public'));
     app.set("view engine", "ejs");
     app.set("views", "./src/views")
 }
